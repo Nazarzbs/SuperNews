31fa059fd2aa024de0effd3e5f3d049805ede01c
Nazar <famina1998@gmail.com>
2025-11-15 13:23:54 +0200

    Initial Commit

---
+++ SuperNews/SuperNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperNewsApp.swift
+//  SuperNews
+//
+//  Created by Nazar on 15.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct SuperNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
